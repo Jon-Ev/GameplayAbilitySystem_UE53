8c57b90ece95470cfaf853583ec32d1b07653aad
Jon-Ev <mister_e_of_life@yahoo.co.uk>
2024-10-23 17:44:06 +0100

    Initial enemy highlighting using stencil buffer

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,11 +5,19 @@
 
 #include "EnhancedInputSubsystems.h"
 #include <EnhancedInputComponent.h>
+#include <Interaction/EnemyInterface.h>
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	// Used for multiplayer server-client synchronisation
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -52,5 +60,65 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	{
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 * Line trace from cursor. There are several scenarios:
+	 * A. LastActor is null && ThisActor is null
+	 *		- Do nothing
+	 * B. LastActor is null && ThisActor is valid
+	 *		- Hightlight ThisActor
+	 * C. LastActor is valid && ThisActor is null
+	 *		- Unhightlight LastActor
+	 * D. Both actors are valid, but LastActor != ThisActor
+	 *		- Unhightlight LastActor, Highlight ThisActor
+	 * E. Both actors are valid and are the same
+	 *		- Do nothing
+	 */
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A - Both null, do nothing
+		}
+	}
+	else
+	{
+		// LastActor is valid
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnhighlightActor();
+		}
+		else
+		{
+			// Both actors valid
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnhighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case E - Same actor, do nothing
+			}
+		}
 	}
 }
