282325aa8e858c145e4ecea2548a62c43bdc51fd
Jon-Ev <mister_e_of_life@yahoo.co.uk>
2024-10-28 17:51:29 +0000

    Added game effects and multiple items

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright EMC2
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
